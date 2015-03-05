6f1045154eba427392c4a1f3426891c2958a9fca
Min  Xing <mxing@searles224-02w7.campus.bowdoin.edu>
2015-03-05 14:14:35 -0500

    added intersection methods

--- orthosegintersection/geom.c
+++ orthosegintersection/geom.c
@@ -13,7 +13,7 @@
  */
 int signed_area2D(point2D a, point2D b, point2D c) {
 
-  return 1; 
+  return (b.x - a.x) * (c.y - a.y) - (c.x -a.x) * (b.y - a.y);
 }
 
 
@@ -22,7 +22,7 @@ int signed_area2D(point2D a, point2D b, point2D c) {
 /* return 1 if p,q,r collinear, and 0 otherwise */
 int collinear(point2D p, point2D q, point2D r) {
   
-  return 1; 
+  return signed_area2D(p, q, r) == 0;
 }
 
 
@@ -31,5 +31,61 @@ int collinear(point2D p, point2D q, point2D r) {
 /* return 1 if c is  strictly left of ab; 0 otherwise */
 int left (point2D a, point2D b, point2D c) {
   
-  return 1; 
+  return signed_area2D(a, b, c) > 0;
+}
+
+
+//return 1 if s1 and s2 intersect
+int intersect(segment2D s1, segment2D s2) {
+    if (intersect_proper(s1, s2)) {
+        return 1;
+    } else if ( between(s1.start, s1.end, s2.start) ||
+               between(s1.start, s1.end, s2.end) ||
+               between(s2.start, s2.end, s1.start) ||
+               between(s2.start, s2.end, s1.end)) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+// exclusive or.
+int Xor(int x, int y) {
+    return !x ^ !y;
+}
+
+//return 1 if s1 and s2 intersect at a proper point (point that’s interior to both)
+int intersect_proper (segment2D s1, segment2D s2) {
+    if (collinear(s1.start, s1.end, s2.start) ||
+        collinear(s1.start, s1.end, s2.end) ||
+        collinear(s2.start, s2.end, s1.start) ||
+        collinear(s2.start, s2.end, s1.end)) {
+        return 0;
+    }
+    
+    return Xor(left(s1.start, s1.end, s2.start), left(s1.start, s1.end, s2.end)) &&
+            Xor(left(s2.start, s2.end, s1.start), left(s2.start, s2.end, s1.end));
+}
+
+//return 1 if  s1 and s2 intersect improperly, i.e. the intersection point is the endpoint of one or both segments
+int intersect_improper (segment2D s1,  segment2D s2) {
+    if (intersect(s1, s2) && !intersect_proper(s1, s2)) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+// Check whether point c is between a and b
+int between(point2D a, point2D b, point2D c) {
+    if (!collinear(a, b, c)) {
+        return 0;
+    }
+    if (a.x != b.x) {
+        return ((a.x <= c.x) && (c.x <= b.x)) ||
+                ((a.x >= c.x) && (c.x >= b.x));
+    } else {
+        return ((a.y <= c.y) && (c.y <= b.y)) ||
+                ((a.y >= c.y) && (c.y >= b.y));
+    }
 }
